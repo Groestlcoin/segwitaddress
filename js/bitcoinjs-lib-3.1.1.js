dc343ae1bb94604a6a977dff285fa7caf8e4d5ba
HashEngineering <hashengineeringsolutions@gmail.com>
2017-11-22 08:53:06 -0800

    Fix text issues and update metropy.js for GRS

--- js/bitcoinjs-lib-3.1.1.js
+++ js/bitcoinjs-lib-3.1.1.js
@@ -11528,7 +11528,7 @@ Block.fromHex = function (hex) {
 }
 
 Block.prototype.getHash = function () {
-  return bcrypto.hash256(this.toBuffer(true))
+  return bcrypto.groestl(this.toBuffer(true))
 }
 
 Block.prototype.getId = function () {
