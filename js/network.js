305bb4b347bd05e1554a7c4f7e78856d2f53d549
HashEngineering <hashengineeringsolutions@gmail.com>
2018-07-08 16:12:42 -0700

    Use proper urls for sweeping and posting (they were reversed)

--- js/network.js
+++ js/network.js
@@ -12,14 +12,18 @@ function headerHtml() {
     return '<h1>Groestlcoin SegWit Paper Wallet' + (isTestNet ? '<span style="color:red;"> (TestNet)</span>' : '') + '</h1>';
 }
 
+function headerHtml_bech32() {
+    return '<h1>Groestlcoin Bech32 Paper Wallet' + (isTestNet ? '<span style="color:red;"> (TestNet)</span>' : '') + '</h1>';
+}
+
 function apiDomain() {
-    return isTestNet ? 'https://chainz.cryptoid.info/grs/api.dws?key=d47da926b82e' : 'https://chainz.cryptoid.info/grs-test/api.dws?key=d47da926b82e';
+    return !isTestNet ? 'https://chainz.cryptoid.info/grs/api.dws?key=d47da926b82e' : 'https://chainz.cryptoid.info/grs-test/api.dws?key=d47da926b82e';
 }
 
 function apiDomain2() {
-    return isTestNet ? 'https://groestlsight.groestlcoin.org' : 'https://groestlsight.groestlcoin.org';
+    return !isTestNet ? 'https://groestlsight.groestlcoin.org' : 'https://groestlsight.groestlcoin.org';
 }
 
 function explorerDomain() {
-    return isTestNet ? 'https://chainz.cryptoid.info/grs/' : 'https://chainz.cryptoid.info/grs-test/';
+    return !isTestNet ? 'https://chainz.cryptoid.info/grs/' : 'https://chainz.cryptoid.info/grs-test/';
 }
